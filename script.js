d9c14a5fb94ef1db8113c0ac7f222074c2e4e54c
CinLo82 <cinlos3382@gmail.com>
2022-06-20 08:56:03 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// Obtener control sobre los elementos
+let input = document.getElementById('texto');
+let boton;
+let lista;
+
+// Tener la información guardada
+let data = [
+  {
+    id: 1, // para identificar
+    titulo: 'Hacer la comida',
+    check: false,
+    show: true, // para borrado lógico
+  },
+];
+
+// Crear el evento del boton
+
+// Guardar en el objeto data, la info del elemento nuevo
+
+// Crear funciones:
+// ==> Marcar un elemento como realizado (check = true)
+
+// ==> Eliminar elemento
